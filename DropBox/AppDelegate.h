4453839edf06e36de2bc15217c87893d00849225
Viktor Chibotaru <vcibotaru@gmail.com>
2013-12-31 16:14:06 +0200

    Initial Commit

---
+++ DropBox/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DropBox
+//
+//  Created by Viktor Chibotaru on 12/31/13.
+//  Copyright (c) 2013 Viktor Chibotaru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DropBox/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DropBox
+//
+//  Created by Viktor Chibotaru on 12/31/13.
+//  Copyright (c) 2013 Viktor Chibotaru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
